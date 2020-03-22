e8d08c7e03024aadbfe7a35f8d237d2f8abde11c
Joel Nider <joel@nider.org>
2020-03-22 14:39:48 -0700

    belady: implement 'lookup' for loaded values
    Also move WT state to be global.
    
    Signed-off-by: Joel Nider <joel@nider.org>

--- replacement/belady.cpp
+++ replacement/belady.cpp
@@ -11,27 +11,41 @@ algorithms can be measured.
 #include "ooo_cpu.h"
 #include "wiredtiger.h"
 
+// make WiredTiger global state
+	WT_SESSION *session;
+	char table_name[1024];
+
 // insert a value
-static void insert(WT_SESSION *session, char *table, uint64_t addr, uint64_t timestamp)
+static void insert(WT_CURSOR *cursor, uint64_t addr, uint64_t timestamp)
 {
-	WT_CURSOR *cursor;
-	int ret;
 	char key[24];
 	char value[24];
 
-	//printf("Inserting 0x%lx %lu\n", addr, timestamp);
-	ret = session->open_cursor(session, table, NULL, NULL, &cursor);
-	if (ret != 0)
-	{
-		printf("Error opening cursor during insert\n");
-		return;
-	}
-	snprintf(key, 24, "%lu", addr);
+	snprintf(key, 24, "%lx", addr);
 	snprintf(value, 24, "%lu", timestamp);
+	//printf("Inserting %s %s\n", key, value);
 	cursor->set_key(cursor, key);
 	cursor->set_value(cursor, value);
 	cursor->insert(cursor);
 	cursor->close(cursor);
+}
+
+static int lookup(WT_CURSOR *cursor, uint64_t addr, uint64_t *timestamp)
+{
+	char key[24];
+	const char *value;
+
+	snprintf(key, 24, "%lx", addr);
+    cursor->set_key(cursor, key);
+   int ret = cursor->search(cursor);
+	if (ret < 0)
+	{
+		printf("key %s not found\n", key);
+		return -1;
+	}
+    cursor->get_value(cursor, &value);
+	*timestamp = strtoul(value, NULL, 10);
+	return 0;
 }
 
 // initialize replacement state
@@ -43,7 +57,7 @@ void CACHE::llc_initialize_replacement()
 	long unsigned int stores=0;
 
 	WT_CONNECTION *conn;
-	WT_SESSION *session;
+	WT_CURSOR *cursor;
 
 	char home[] = "/home/joel/projects/ChampSim/db";
 
@@ -63,7 +77,6 @@ void CACHE::llc_initialize_replacement()
 	}
 
 	// create a table
-	char table_name[1024];
 	snprintf(table_name, 127, "table:%s", "gamess");
 	int ret = session->create(session, table_name, "key_format=S,value_format=S");
 	if (ret != 0)
@@ -81,6 +94,7 @@ void CACHE::llc_initialize_replacement()
 		ins++;
 	}
 
+
 	printf("Loading %lu simulation instructions\n", ooo_cpu[0].simulation_instructions);
 	while (fread(&in, sizeof(in), 1, ooo_cpu[0].trace_file))
 	{
@@ -89,8 +103,19 @@ void CACHE::llc_initialize_replacement()
 			if (in.source_memory[s])
 			{
 				//printf("%i:   LOAD <- 0x%lx\n", i, in.source_memory[s]);
-				insert(session, table_name, in.source_memory[s], ins);
+				if (session->open_cursor(session, table_name, NULL, NULL, &cursor) == 0)
+					insert(cursor, in.source_memory[s], ins);
+				else
+					printf("Error inserting\n");
 				loads++;
+/*
+				if (session->open_cursor(session, table_name, NULL, NULL, &cursor) == 0)
+				{
+					uint64_t ts;
+					if (lookup(cursor, in.source_memory[s], &ts) == 0)
+						printf("Found %lx at %lu\n", in.source_memory[s], ts); 
+				}
+*/
 			}
 		}
 
@@ -99,7 +124,13 @@ void CACHE::llc_initialize_replacement()
 			if (in.destination_memory[d])
 			{
 				//printf("%i:   STORE -> 0x%lx\n", i, in.destination_memory[d]);
-				insert(session, table_name, in.destination_memory[d], ins);
+				ret = session->open_cursor(session, table_name, NULL, NULL, &cursor);
+				if (ret != 0)
+				{
+					printf("Error opening cursor during insert\n");
+					return;
+				}
+				insert(cursor, in.destination_memory[d], ins);
 				stores++;
 			}
 		}
@@ -114,18 +145,6 @@ void CACHE::llc_initialize_replacement()
 	fseek(ooo_cpu[0].trace_file, 0L, SEEK_SET);
 
 	printf("Saw %lu loads and %lu stores\n", loads, stores);
-/*
-	// dump the map
-	printf("Dump\n");
-	for (mem_map::iterator iter=memory_accesses.begin(); iter != memory_accesses.end(); iter++)
-	{
-		printf("0x%lx: ", iter->first);
-		access_vector& v = memory_accesses[iter->first];
-		for (unsigned int idx=0; idx < v.size(); idx++)
-			printf("%lu, ", v[idx]);
-		printf("\n");
-	}
-*/
 }
 
 // find replacement victim
@@ -133,6 +152,10 @@ uint32_t CACHE::llc_find_victim(uint32_t cpu, uint64_t instr_id, uint32_t set,
 	const BLOCK *current_set, uint64_t ip, uint64_t full_addr, uint32_t type)
 {
 	uint32_t way = 0;
+	uint32_t best_way = 0;
+	WT_CURSOR *cursor;
+	uint64_t timestamp;
+	uint64_t best_timestamp = 0;
 
 	// baseline LRU
 	//return lru_victim(cpu, instr_id, set, current_set, ip, full_addr, type); 
@@ -144,12 +167,36 @@ uint32_t CACHE::llc_find_victim(uint32_t cpu, uint64_t instr_id, uint32_t set,
 			return way;
 	}
 
+
 	printf("What are my options:\n");
 	for (way=0; way<NUM_WAY; way++)
 	{
-		printf("Way %i = 0x%lx\n", way, block[set][way].full_addr);
+		printf("Full Address=%lx address=%lx\n", block[set][way].full_addr, block[set][way].address);
+		uint64_t addr = block[set][way].address;
+
+		// look up the next time to use
+		if (session->open_cursor(session, table_name, NULL, NULL, &cursor) != 0)
+		{
+			printf("Error opening cursor during insert\n");
+			return 0;
+		}
+		printf("Looking up %i = 0x%lx\n", way, addr);
+		fflush(stdout);
+
+		// if it is not in the database, it is never reused
+		if (lookup(cursor, addr, &timestamp) == -1)
+			return way;
+
+		printf("Way %i = 0x%lx @ %lu\n", way, addr, timestamp);
+		if (timestamp > best_timestamp)
+		{
+			best_timestamp = timestamp;
+			best_way = way;
+		}
 	}
-	return 0;
+
+	printf("Replacing %i @ %lu\n", best_way, best_timestamp);
+	return best_way;
 }
 
 // called on every cache hit and cache fill
